004dd9787b0821e3dec9238d3933ccf92868c1f4
Josh Wright <josh.wright100@gmail.com>
2022-06-16 16:29:32 +0200

    Fix module 4

--- ssg/content.py
+++ ssg/content.py
@@ -41,5 +41,5 @@ class Content(Mapping):
         data = {}
         for key, value in self.data.items():
             if key != "content":
-                data["key"] = value
+                data[key] = value
         return str(data)
